2445a4f92e6b6b9e3cbd7d636bfb1bb0e40749f2
Konstantin-create <schahov.konst@gmail.com>
2022-07-24 17:05:06 +0300

    Commit

--- client/main.py
+++ client/main.py
@@ -1,5 +1,6 @@
 # Imports
 import os
+from getpass4 import getpass
 from modules import server, user
 
 # Initialize root dir path
@@ -10,7 +11,26 @@ connection = server.ServerConnection(base_dir)
 user = user.User(base_dir)
 
 
-# Main loop
-while True:
+# Functuons
+def not_logged():
+    command = int(input("""Until you are logged into your account. Select menu item\n    1 - Login\n    2) Registration"""))
+    if command-1:
+        login()
+    else:
+        registration()
+
+def login():
+    username = str(input('Enter username: '))
+    password = str(getpass('Enter password: '))
+    server
+
+def registration():
     pass
 
+
+# Main loop
+while True:
+    if user.is_logged():
+        pass
+    else:
+        pass
