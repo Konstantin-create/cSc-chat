088c8d569c000fa3697eb86fd0f39fd7c7fb8549
Konstantin-create <schahov.konst@gmail.com>
2022-07-24 13:38:37 +0300

    Remove client/requirements.txt

--- client/main.py
+++ client/main.py
@@ -1,6 +1,16 @@
+# Imports
 import os
-from modules import server
+from modules import server, user
 
+# Initialize root dir path
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
+# Initialize classes
 connection = server.ServerConnection(base_dir)
+user = user.User(base_dir)
+
+
+# Main loop
+while True:
+    pass
+
--- client/modules/user.py
+++ client/modules/user.py
@@ -1,3 +1,5 @@
+import json
+from loguru import logger
 from hashlib import sha256
 
 
@@ -6,13 +8,50 @@ def password_hash(password):
 
 
 class User:
-    def __init__(self, base_dir):
+    def __init__(self, base_dir: str):
         self.root = base_dir
         self.cdata = f'{self.root}/cdata'
+    
+    # Working with files
+    def get_cdata(self):
+        """Function to get all data from cdata folder. Return dict {'session': dict, 'chats': list}"""
+        try:
+            with open(f'{self.cdata}/session.session', 'r') as file:
+                session = json.load(file)
+            with open(f'{self.cdata}/chats.json', 'r') as file:
+                chats = json.load(file)
+            return {'session': session, 'chats': chats}
+        except Exception as e:
+            logger.error(e)
+            return {'session': None, 'chats': None}
+    
+    def set_chats(self, chat_list: list):
+        """Function to set chat list. Get chat_list param. Return dict {'recorded': bool}"""
+        try:
+            with open(f'{self.cdata}/chats', 'w') as file:
+                json.dump(chat_list, file)
+            return {'recorded': True}
+        except Exception as e:
+            logger.error(e)
+            return {'recorded', False}
+    
+    def set_session(self, session: dict):
+        """Function to write info in session file. Get session(dict) param. Return dict {'recorded': bool}"""
+        try:
+            with open(f'{self.cdata}/session', 'w') as file:
+                json.dump(session, file)
+            return recorded
+        except Exception as e:
+            logger.error(e)
+            return {'recorded': False}
 
-    def get_cdata_session(self):
-        with open(f'{self.cdata}/session', 'w') as file:
-            session = file.read()
-        with open(f'{self.cdata}/chats.json') as file:
-            chats = file.read()
+    def change_session_key(self, key:str, value: any):
+        """Function to change some sesssion values in file. Get key, value params. Return dict {'recorded': bool}"""
+        try:
+            session = get_cdata()['session']
+            session[key] = value
+            set_session(session)
+        except Exception as e:
+            logger.error(e)
+            return {'recorded': False}
 
